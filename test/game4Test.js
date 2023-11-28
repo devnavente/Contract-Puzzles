5c9a86d100bdacbbba679f8fd26ab4dd1b56ea38
Benavente <lbenavente@proton.me>
2023-11-29 00:55:52 +0100

    5 games won

--- test/game4Test.js
+++ test/game4Test.js
@@ -6,14 +6,18 @@ describe('Game4', function () {
     const Game = await ethers.getContractFactory('Game4');
     const game = await Game.deploy();
 
-    return { game };
+    const signer1 = ethers.provider.getSigner(0);
+    const signer2 = ethers.provider.getSigner(1);
+
+    return { game, signer1, signer2 };
   }
   it('should be a winner', async function () {
-    const { game } = await loadFixture(deployContractAndSetVariables);
+    const { game, signer1, signer2 } = await loadFixture(deployContractAndSetVariables);
 
     // nested mappings are rough :}
+    await game.connect(signer2).write(signer1.getAddress()); // nested[x][msg.sender]
 
-    await game.win();
+    await game.win(signer2.getAddress()); // nested[msg.sender][x]
 
     // leave this assertion as-is
     assert(await game.isWon(), 'You did not win the game');
--- test/game5Test.js
+++ test/game5Test.js
@@ -11,9 +11,32 @@ describe('Game5', function () {
   it('should be a winner', async function () {
     const { game } = await loadFixture(deployContractAndSetVariables);
 
-    // good luck
+    const threshold = '0x00FfFFfFFFfFFFFFfFfFfffFFFfffFfFffFfFFFf';
 
-    await game.win();
+    let valid = false;
+    let wallet = null;
+    let address = null;
+
+    while (!valid) {
+      wallet = ethers.Wallet.createRandom();
+      address = await wallet.getAddress();
+
+      if (address < threshold) {
+        valid = true;
+      }
+
+    }
+
+    wallet = wallet.connect(ethers.provider);
+    const signer = await ethers.provider.getSigner(0);
+
+    // sends some ether(needed for gas fees on the upcoming function call)
+    await signer.sendTransaction({
+      to: address,
+      value: ethers.utils.parseEther('200')
+    })
+
+    await game.connect(wallet).win();
 
     // leave this assertion as-is
     assert(await game.isWon(), 'You did not win the game');
